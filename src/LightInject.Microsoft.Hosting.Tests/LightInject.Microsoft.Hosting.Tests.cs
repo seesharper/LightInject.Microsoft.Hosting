00ac6994ca84e3a95736a6a5f0d4708f309ea50f
Bernhard Richter <bernhard.richter@gmail.com>
2024-11-10 19:19:20 +0100

    Added test for issue 31

--- src/LightInject.Microsoft.Hosting.Tests/LightInject.Microsoft.Hosting.Tests.cs
+++ src/LightInject.Microsoft.Hosting.Tests/LightInject.Microsoft.Hosting.Tests.cs
@@ -26,7 +26,7 @@ public class HostTests
 
     [Fact]
     public void ShouldNotHaveVarianceEnabledByDefault()
-    {        
+    {
         var host = new HostBuilder().UseLightInject(r => r.Register<Base>().Register<Derived>()).Build();
         var instances = host.Services.GetServices<Base>();
         Assert.Single(instances);
@@ -39,8 +39,31 @@ public class HostTests
         var instances = host.Services.GetServices<Base>();
         Assert.Equal(2, instances.Count());
     }
+
+    [Fact]
+    public void Issue31()
+    {
+        var host = new HostBuilder().UseLightInject(r =>
+        {
+            r
+            .Register<IFoo, Foo1>("Foo1")
+            .Register<IFoo, Foo2>("Foo2")
+            .Register<IFoo>(factory => new Foo3());
+
+        }, options => options.EnableVariance = false).Build();
+
+        var foo = host.Services.GetRequiredService<IFoo>();
+
+        Assert.IsType<Foo3>(foo);
+    }
 }
 
 public class Base { }
 
 public class Derived : Base { }
+
+public class Foo1 : IFoo { }
+
+public class Foo2 : IFoo { }
+
+public class Foo3 : IFoo { }
