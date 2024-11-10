77635fe0bb3e4a11aecdc5ea498305f467b57049
Bernhard Richter <bernhard.richter@gmail.com>
2024-11-10 19:08:42 +0100

    Added func to configure options and set EnableVariance to default false

--- src/LightInject.Microsoft.Hosting.Tests/LightInject.Microsoft.Hosting.Tests.cs
+++ src/LightInject.Microsoft.Hosting.Tests/LightInject.Microsoft.Hosting.Tests.cs
@@ -2,23 +2,45 @@ using System;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using System.Linq;
 
-namespace LightInject.Microsoft.Hosting.Tests
+namespace LightInject.Microsoft.Hosting.Tests;
+
+public class HostTests
 {
-    public class UnitTest1
+    [Fact]
+    public void ShouldResolveService()
     {
-        [Fact]
-        public void ShouldResolveService()
-        {
-            var host = new HostBuilder().UseLightInject().Build();
-            var foo = host.Services.GetRequiredService<IFoo>();
-        }
+        var host = new HostBuilder().UseLightInject().Build();
+        var foo = host.Services.GetRequiredService<IFoo>();
+        Assert.NotNull(foo);
+    }
 
-        [Fact]
-        public void ShouldRegisterAndResolveService()
-        {
-            var host = new HostBuilder().UseLightInject(r => r.Register<IBar, Bar>()).Build();
-            var foo = host.Services.GetRequiredService<IFoo>();
-        }
+    [Fact]
+    public void ShouldRegisterAndResolveService()
+    {
+        var host = new HostBuilder().UseLightInject(r => r.Register<IBar, Bar>()).Build();
+        var bar = host.Services.GetRequiredService<IBar>();
+        Assert.NotNull(bar);
+    }
+
+    [Fact]
+    public void ShouldNotHaveVarianceEnabledByDefault()
+    {        
+        var host = new HostBuilder().UseLightInject(r => r.Register<Base>().Register<Derived>()).Build();
+        var instances = host.Services.GetServices<Base>();
+        Assert.Single(instances);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToEnableVariance()
+    {
+        var host = new HostBuilder().UseLightInject(r => r.Register<Base>().Register<Derived>(), options => options.EnableVariance = true).Build();
+        var instances = host.Services.GetServices<Base>();
+        Assert.Equal(2, instances.Count());
     }
 }
+
+public class Base { }
+
+public class Derived : Base { }
